34704968c99763f195bcf0bad90ce3a1b286b78c
Arij-Aladel <arij.aladel@gmail.com>
2025-08-01 00:10:00 +0200

    create

--- src/main.py
+++ src/main.py
@@ -5,6 +5,8 @@ from routes import base, data
 from motor.motor_asyncio import AsyncIOMotorClient
 from helpers.config import get_settings
 
+from stores.LLMProviderFactory import LLMProviderFactory
+from stores.LLMEnums import LLMEnums
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -12,8 +14,17 @@ async def lifespan(app: FastAPI):
     settings = get_settings()
     app.mongo_conn = AsyncIOMotorClient(settings.MONGODB_URL)
     app.db_client = app.mongo_conn[settings.MONGODB_DATABASE]
+    llm_privider_factory = LLMProviderFactory(settings)
 
+    #generate LLM client based on settings
+    app.generation.client = llm_privider_factory.create(provider=settings.GENERATION_BACKEND)
+    app.generation.client.set_generation_model(model_id=settings.GENERATION_MODEL_ID)
 
+    #embedding LLM client
+    app.embedding.client = llm_privider_factory.create(provider=settings.EMBEDDING_BACKEND)
+    app.embedding.client.set_embedding_model(
+        model_id=settings.EMBEDDING_MODEL_ID,
+        embedding_size=settings.EMBEDDING_SIZE)
     yield  # Application runs here
 
     # Shutdown logic
---
+++ src/stores/llm/providers/__init__.py
@@ -0,0 +1,4 @@
+from .CoHereProvider import CoHereProvider
+from .OpenAIProvider import OpenAIProvider
+from .LLMInterface import LLMInterface
+from .LLMEnums import LLMEnums
