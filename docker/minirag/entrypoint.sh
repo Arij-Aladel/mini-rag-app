db0ff59b0c6362469191bd32f25490bfbca14a2d
Arij-Aladel <arij.aladel@gmail.com>
2025-11-05 05:29:22 +0100

    feat: add Prometheus metrics setup and middleware for monitoring HTTP requests

--- docker/minirag/entrypoint.sh
+++ docker/minirag/entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -e
 
-echo "Running database migrations..."
+echo "Running database migrations............."
 cd /app/models/db_schemes/minirag/
 alembic upgrade head
 cd /app
+echo "Finish database migrations............."
+
+exec "$@"
